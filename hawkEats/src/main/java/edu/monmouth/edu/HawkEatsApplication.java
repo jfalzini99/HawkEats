ac959d9655274a17ac6322a11f0e92c3467bfb93
Muhammad Fahad <muhammadfahad@Muhammads-MacBook-Pro.local>
2021-02-14 21:15:08 -0500

    Add initial proj

---
+++ hawkEats/src/main/java/edu/monmouth/edu/HawkEatsApplication.java
@@ -0,0 +1,13 @@
+package edu.monmouth.edu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HawkEatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HawkEatsApplication.class, args);
+	}
+
+}
---
+++ hawkEats/src/test/java/edu/monmouth/edu/HawkEatsApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.monmouth.edu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HawkEatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
